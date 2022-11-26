aa12344ef00e872f9ed8ab3e02a47d245a103df4
Italovini223 <italovinicius2018@gmail.com>
2022-11-26 12:39:52 -0300

    ✨ feat: add route to create a new user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {userName} = request.headers;
+
+  const user = users.find(user => user.userName === userName);
+
+  if(!user){
+    return response.status(400).json({error: 'User not found'});
+  }
+
+  request.user = user;
+  return next();
+  
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, userName} = request.body;
+
+  const userNameExistent = users.some(user => user.userName === userName);
+
+  if(userNameExistent) {
+    return response.status(400).json({error: 'User name already exists'});
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    userName,
+    todos: []
+  });
+
+
+  return response.status(200).send();
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
